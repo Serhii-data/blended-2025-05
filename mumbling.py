a1406524d30406712fc4338cfe7dec4dcc2edfa0
Serhii-data <fedchuk.s@gmail.com>
2025-05-20 22:13:22 +0300

    commit-2025-05-20

--- mumbling.py
+++ mumbling.py
@@ -1,4 +1,5 @@
 '''
+
 This time no story, no theory. The examples below show you how to write function accum:
 
 Examples:
@@ -6,6 +7,7 @@ accum("abcd") -> "A-Bb-Ccc-Dddd"
 accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.
+
 '''
 
 # def accum(string: str) -> str:
